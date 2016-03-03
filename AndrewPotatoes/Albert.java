9a53e44a26fd05a0c7b74b9d861ada13e833a951
Andrew Luo <aluo8@students.d125.org>
2016-03-03 14:19:24 -0600

    1

---
+++ AndrewPotatoes/Albert.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Albert here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Albert
+{
+    // instance variables - replace the example below with your own
+    private int height;
+    private int weight;
+    /**
+     * Constructor for objects of class Albert
+     */
+    public Albert()
+    {
+       weight = 200;
+       height = 65;
+    }
+
+    
+}
